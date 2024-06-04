f195a51825bb348ce656599f00312f98a56464f2
estymate <aleksandar@localhost.localdomain>
2024-06-04 14:16:34 +0200

    initial push

---
+++ ImmobilienverwaltungWeb/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ImmobilienverwaltungWeb.Models;
+using ImmobilienverwaltungWeb.Repositories;
+
+namespace ImmobilienverwaltungWeb.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        ImmobilienRepository repo = new ImmobilienRepository();
+        List<Immobilien> myImmobilien = repo.GetAllImmobiliens();
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ ImmobilienverwaltungWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ImmobilienverwaltungWeb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
